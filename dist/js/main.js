3085588952787c2dfbdde3c101284c2584bffff4
OhadShalev <shalev.ohad@gmail.com>
2021-09-26 20:51:40 +0300

    add mobile responsive suport and make some graphic improvments

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,4 +1,4 @@
-let deadeline = new Date(2021, 9, 20, 19, 0, 0);
+let deadeline = new Date(2021, 7, 20, 19, 0, 0);
 let interval;
 
 $(document).ready(function () {
@@ -8,7 +8,6 @@ $(document).ready(function () {
 
 function countdown() {
   let timer_ = deadeline.getTime() / 1000;
-  console.log(timer_)
   let countdownDom = document.querySelector(".flipdown");
   let flipdown = new FlipDown(timer_, 'countdown', {
     theme: "light",
@@ -17,25 +16,6 @@ function countdown() {
     .start()
     .ifEnded(() => {
       console.log("The countdown has ended! event begin!");
-      countdownDom.innerHTML = '<H1 class="timepassed">התחלנו!!</H1>'
+      countdownDom.innerHTML = '<H1 class="timepassed">התחלנו!!!</H1>'
     });
-  // 
-  // let current = new Date();
-  // let diff = deadeline - current;
-  // let days = Math.floor(diff / (1000 * 60 * 60 * 24)) + "";
-  // let hours = Math.floor((diff / (1000 * 60 * 60)) % 24) + "";
-  // let minutes = Math.floor((diff / (1000 * 60)) % 60) + "";
-  // let seconds = Math.floor((diff / (1000)) % 60) + "";
-
-  // countdownDom.innerHTML = `
-  //   <div data-content="ימים">${days.length === 1 ? `0${days}` : days}</div>
-  //   <div data-content="שעות">${hours.length === 1 ? `0${hours}` : hours}</div>
-  //   <div data-content="דקות">${minutes.length === 1 ? `0${minutes}` : minutes}</div>
-  //   <div data-content="שניות">${seconds.length === 1 ? `0${seconds}` : seconds}</div>
-  // `;
-
-  // if (diff < 0) {
-  //   clearInterval(interval);
-  //   countdownDom.innerHTML = '<H1 class="timepassed">התחלנו!!</H1>'
-  // }
 }
